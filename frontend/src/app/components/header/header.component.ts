45f79aa62a4d6f62064b23e98389a9f3b0ed6910
luis782006 <luisfelipe782006@gmail.com>
2025-05-24 16:28:38 -0300

    Estilos en Home y Header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { Menubar } from 'primeng/menubar';
+import { TooltipModule } from 'primeng/tooltip';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
-  imports: [Menubar],
+  standalone: true,
+  imports: [
+    CommonModule,
+    Menubar, 
+    TooltipModule,
+    RippleModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  items: MenuItem[] = []; // Propiedad items necesaria para el menú
+}
