5cf830ac8ee6181a721a3263e5af62a5f0f4aa45
luis782006 <luisfelipe782006@gmail.com>
2025-05-24 21:52:49 -0300

    Modificacion footer en app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ConfirmDialogModule, ToastModule, HeaderComponent],
+  imports: [RouterOutlet, ConfirmDialogModule, ToastModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ConfirmationService, MessageService],
