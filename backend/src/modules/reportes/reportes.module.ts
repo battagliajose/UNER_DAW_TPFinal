66b1b7b33d39fdd346c638532638e60ad392763a
cristianseltenreich <cristianseltenreich@gmail.com>
2025-05-25 13:17:56 -0300

    configuración correcta de reportes.module y agrego el módulo reporte al app.module

--- backend/src/modules/reportes/reportes.module.ts
+++ backend/src/modules/reportes/reportes.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportesService } from './services/reportes.service';
+import { ReportesController } from './controllers/reportes.controller';
+import { Encuesta } from '../encuestas/entities/encuesta.entity';
+import { Pregunta } from '../encuestas/entities/pregunta.entity';
+import { RespuestaEncuesta } from '../respuestas/entities/respuesta-encuesta.entity';
+import { RespuestaAbierta } from '../respuestas/entities/respuesta-abierta.entity';
+import { RespuestaOpcion } from '../respuestas/entities/respuesta-opcion.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Encuesta,
+      Pregunta,
+      RespuestaEncuesta,
+      RespuestaAbierta,
+      RespuestaOpcion,
+    ]),
+  ],
+  providers: [ReportesService],
+  controllers: [ReportesController],
+  exports: [ReportesService],
+})
 export class ReportesModule {}
