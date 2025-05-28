f504e6c035595e43c873fa45468a0bc32bb35d8f
Luis <luisfelipe782006@gmail.com>
2025-05-28 11:45:27 -0300

    Docker

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, ConfirmDialogModule, ToastModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
