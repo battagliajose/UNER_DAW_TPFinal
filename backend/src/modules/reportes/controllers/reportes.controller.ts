863423955b0cf8ea2fa652a0776613cf709d23dd
cristianseltenreich <cristianseltenreich@gmail.com>
2025-05-30 19:27:38 -0300

    agrego csv-reporte.service y modifico para incluirlo controller.service y lo importo en reportes module

--- backend/src/modules/reportes/controllers/reportes.controller.ts
+++ backend/src/modules/reportes/controllers/reportes.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { ReportesService } from '../services/reportes.service';
 import { PdfService } from '../services/pdf.service';
+import { CsvReportesService } from '../services/csv-reportes.service';
 import { Response } from 'express';
 
 @Controller('reportes')
@@ -8,6 +9,7 @@ export class ReportesController {
   constructor(
     private readonly reportesService: ReportesService,
     private readonly pdfService: PdfService,
+    private readonly csvReportesService: CsvReportesService,
   ) {}
 
   @Get('/pdf/:id/:codigo')
@@ -21,6 +23,19 @@ export class ReportesController {
       codigo,
     );
     return this.pdfService.generarPDF(datosReporte, res);
+  }
+
+  @Get('/csv/:id/:codigo')
+  async generarCSV(
+    @Param('id') id: number,
+    @Param('codigo') codigo: string,
+    @Res() res: Response,
+  ) {
+    const datosReporte = await this.reportesService.generarReporteResultados(
+      id,
+      codigo,
+    );
+    return this.csvReportesService.generarCSV(datosReporte, res);
   }
 
   @Get(':id/:codigo')
---
+++ backend/src/modules/reportes/services/csv-reportes.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class CsvReportesService {
+  async generarCSV(datosReporte: any, res: Response) {
+    const resultados =
+      datosReporte.resumenEstadistico?.resultadosProcesados || [];
+
+    // Definir encabezados
+    const encabezados = [
+      'Tipo Pregunta',
+      'Texto Pregunta',
+      'Respuesta',
+      'Cantidad Veces Seleccionada',
+      'Porcentaje Selección',
+    ];
+    let contenidoCSV = encabezados.join(',') + '\n';
+
+    resultados.forEach((pregunta) => {
+      if (pregunta.tipoPregunta === 'ABIERTA') {
+        pregunta.respuestas.forEach((respuesta) => {
+          contenidoCSV += `"${pregunta.tipoPregunta}","${pregunta.textoPregunta}","${respuesta}","",""\n`;
+        });
+      } else {
+        pregunta.opciones.forEach((op) => {
+          contenidoCSV += `"${pregunta.tipoPregunta}","${pregunta.textoPregunta}","${op.textoOpcion}","${op.cantidadVecesSeleccionada}","${op.porcentajeSeleccion}"\n`;
+        });
+      }
+    });
+
+    const filePath = path.join(process.cwd(), 'reporte.csv');
+    fs.writeFileSync(filePath, contenidoCSV);
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename="reporte.csv"');
+    res.send(contenidoCSV);
+  }
+}
