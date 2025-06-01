31d42743b909eb5914f0bab6faca3128319f4603
luis782006 <luisfelipe782006@gmail.com>
2025-06-01 18:11:59 -0300

    Se agregaron atributos

--- frontend/src/app/components/dashboard/dashboard-content/dashboard-content.component.ts
+++ frontend/src/app/components/dashboard/dashboard-content/dashboard-content.component.ts
@@ -5,11 +5,20 @@ import { ButtonModule } from 'primeng/button';
 import { EncuestaService } from '../../../services/encuesta.service';
 import { EncuestaDTO } from '../../../models/encuesta.dto';
 import { delay } from 'rxjs/operators';
+
+
 interface DashboardCard {
   title: string;
-  value: number | string;
+  value: number | string;  
+  esActivo: boolean;
+  esEnviada: boolean;
   icon: string;
   color?: string;
+}
+
+interface NuevaEncuestaDTO extends EncuestaDTO {
+  esActivo: boolean;
+  esEnviada: boolean;
 }
 
 @Component({
@@ -25,7 +34,7 @@ export class DashboardContentComponent {
 
   //variable para poblar el dashboard-content
   encuestas: EncuestaDTO[] = [];
-
+  nuevaEncuesta: NuevaEncuestaDTO[] = [];
   //variable para mostrar el loading
   loading: boolean = true;
  
@@ -38,53 +47,107 @@ export class DashboardContentComponent {
     this.loadEncuestas();
   }
 
+  /**
+   * Carga las encuestas desde el backend
+   * Agrego atributos booleanos a las encuestas
+   * Agrego valores aleatorios a los atributos booleanos
+   * Emito los datos al padre
+   * Actualizo las tarjetas del dashboard
+   */
   public loadEncuestas(): void {
     this.loading = true;
     this.error = null;
 
-    //al metodo se le agrega delay para simular un tiempo de carga
-    this.encuestaService.getAll().pipe(delay(2000)).subscribe({      
+    this.encuestaService.getAll().pipe(delay(1000)).subscribe({      
       next: (data) => {
         this.encuestas = data[0]; 
-        console.log("Encuesta",this.encuestas);
-        this.datosCargados.emit(data[0]);       
+        this.nuevaEncuesta = this.agregarAtributos(this.encuestas);  
+        this.nuevaEncuesta = this.agregarValoresAleatorias(this.nuevaEncuesta);              
+        this.datosCargados.emit(this.nuevaEncuesta);       
         this.updateDashboardCards();
         this.loading = false;
       },
-      error: (err) => {
-        console.error('Revisar el server del backend, Posible caida:', err);
+      error: () => {       
         this.error = 'No se pudieron cargar las encuestas. Intente nuevamente o avise al administrador.';
         this.loading = false;
       }
     });
   }
 
+  /**
+   * Actualiza las tarjetas del dashboard con los valores de las encuestas
+   * Total de encuestas
+   * Total de encuestas activas
+   * Total de encuestas enviadas
+   */
   private updateDashboardCards(): void {
     // Actualizar el total de encuestas
-    this.cards[0].value =this.encuestas.length;
-    //this.cards[1].value = this.encuestas.filter(encuesta => encuesta.estado === 'Enviada').length;    
+    this.cardsTotales.value =this.encuestas.length;
+    this.cardsEstados.value = this.nuevaEncuesta.filter(e => e.esActivo).length;
+    this.cardsEnviadas.value = this.nuevaEncuesta.filter(e => e.esEnviada).length;
+    
   }
   
-  //Tarjetas del dashboard por defecto con valores pero no se renderizan hasta que se cargue el objeto
-  //en el .html se renderizan las tarjetas con los valores del objeto cargado
-  cards: DashboardCard[] = [
+ /**
+ * Agrega atributos booleanas a un array de encuestas.
+ * @param encuestas - Array de objetos EncuestaDTO a modificar
+ * @returns Nuevo array con las propiedades adicionales con sus propiedades
+ * @example codigoRespuesta:"xxxx-xxxx-xxxx-xxxx-xxx"
+ * esActivo:1
+ * esEnviada:0
+ * id:1
+ * nombre:"Encuesta de satisfacción"
+ * preguntas:(2) [{…}, {…}]
+ */
+  private agregarAtributos(encuestas: EncuestaDTO[]) {
+    return encuestas.map(encuesta => ({
+      ...encuesta,
+      //nuevos atributos
+      esActivo:false,
+      esEnviada:false
+    }));
+  }
+
+ /**
+ * Agrega valores aleatorios a los atributos esActivo y esEnviada
+ * @param encuestas - Array de objetos NuevaEncuestaDTO a modificar. 
+ * @returns Nuevo array con las propiedades adicionales con sus propiedades
+ */
+  private agregarValoresAleatorias(encuestas: NuevaEncuestaDTO[])  {
+    return encuestas.map(encuesta => ({
+      ...encuesta,
+      esActivo: Math.random() > 0.5,  // Esto devuelve boolean
+      esEnviada: Math.random() > 0.5
+    }));
+  }
+
+// Tarjetas por defecto para porblarlas y mostralas con los valores del objeto
+  cardsTotales: DashboardCard = 
     { 
       title: 'Total de encuestas', 
       value: 0, 
+      esActivo: false,
+      esEnviada: false,
       icon: 'pi pi-receipt',  
       color: 'var(--color-primary)'    
-    },
+    }
+
+    cardsEstados: DashboardCard = 
     { 
-      title: 'Estados de encuestas', 
+      title: 'Encuestas Activas', 
       value: 0, 
+      esActivo: false,
+      esEnviada: false,
       icon: 'pi pi-wave-pulse',
       color: 'var(--color-primary)'
-    },
+    }
+    cardsEnviadas: DashboardCard = 
     { 
       title: 'Encuestas Enviadas', 
       value: 0, 
+      esActivo: false,
+      esEnviada: false,
       icon: 'pi pi-send',
       color: 'var(--color-primary)'
-    },
-  ];
+    }
 }
