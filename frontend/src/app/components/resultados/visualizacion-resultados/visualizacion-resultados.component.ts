5e1ac3a024cdb8caa9c7d31b2a2125c35443cdea
cristianseltenreich <cristianseltenreich@gmail.com>
2025-06-02 11:50:25 -0300

    inicio creación çomponente padre de visualización resultados, de este componente se podrá acceder a los componentes ---> resultados-encuesta y resumen-estadistico

---
+++ frontend/src/app/components/resultados/visualizacion-resultados/visualizacion-resultados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizacion-resultados',
+  imports: [],
+  templateUrl: './visualizacion-resultados.component.html',
+  styleUrl: './visualizacion-resultados.component.css'
+})
+export class VisualizacionResultadosComponent {
+
+}
